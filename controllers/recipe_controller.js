4bc77651e25b41cbfa1c289b1485f49ce2dce4cd
alexwittwer <wittweral@gmail.com>
2024-02-17 12:40:34 -0500

    update recipe controller

--- controllers/recipe_controller.js
+++ controllers/recipe_controller.js
@@ -6,7 +6,7 @@ const asyncHandler = require("express-async-handler");
 
 exports.recipe_get_all = asyncHandler(async (req, res) => {
   try {
-    const allRecipes = await Recipe.find().exec();
+    const allRecipes = await Recipe.find();
 
     if (!allRecipes) {
       return res.sendStatus(404);
@@ -21,9 +21,9 @@ exports.recipe_get_all = asyncHandler(async (req, res) => {
 
 exports.recipe_get_single = asyncHandler(async (req, res) => {
   try {
-    const recipe = await Recipe.findById(req.params.recipeid)
-      .populate("ingredients")
-      .exec();
+    const recipe = await Recipe.findById(req.params.recipeid).populate(
+      "ingredients"
+    );
 
     if (!recipe) {
       return res.sendStatus(404);
@@ -41,7 +41,7 @@ exports.recipe_create = asyncHandler(async (req, res) => {
     const ingredientNames = req.body.ingredients;
     const ingredientObjects = await Promise.all(
       ingredientNames.map(async (name) => {
-        const ingredient = await Ingredient.findOne({ name }).exec();
+        const ingredient = await Ingredient.findOne({ name });
 
         if (!ingredient) {
           console.error({ message: "Ingredient not found" });
@@ -68,10 +68,45 @@ exports.recipe_create = asyncHandler(async (req, res) => {
   }
 });
 exports.recipe_update = asyncHandler(async (req, res) => {
-  return res.json({ message: "This is the recipe_update controller" });
+  try {
+    const recipe = await Recipe.findById(req.params.recipeid);
+
+    const ingredientNames = req.body.ingredients;
+    const ingredientObjects = await Promise.all(
+      ingredientNames.map(async (name) => {
+        const ingredient = await Ingredient.findOne({ name });
+
+        if (!ingredient) {
+          console.error({ message: "Ingredient not found" });
+          return res.sendStatus(404);
+        }
+        return ingredient;
+      })
+    );
+
+    if (!recipe) {
+      return res.sendStatus(404);
+    }
+
+    recipe.name = req.body.name || recipe.name;
+    recipe.ingredients = ingredientObjects
+      ? ingredientObjects
+      : recipe.ingredients;
+    recipe.category = req.body.category || recipe.category;
+    recipe.instructions = req.body.instructions || recipe.instructions;
+    recipe.servings = req.body.servings || recipe.servings;
+    recipe.time = req.body.time || recipe.time;
+    recipe.favorites = req.body.favorites || recipe.favorites;
+
+    await recipe.save();
+    return res.sendStatus(204);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
 });
 exports.recipe_delete = asyncHandler(async (req, res) => {
-  const recipe = await Recipe.findByIdAndDelete(req.params.recipeid).exec();
+  const recipe = await Recipe.findByIdAndDelete(req.params.recipeid);
 
   if (!recipe) {
     return res.sendStatus(404);
